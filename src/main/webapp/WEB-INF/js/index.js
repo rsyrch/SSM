df8ab297697a8db52e836b5be616caa826a421cf
rsyrch <lc812204114@outlook.com>
2018-05-28 22:43:08 +0800

    首页修改

--- src/main/webapp/WEB-INF/js/index.js
+++ src/main/webapp/WEB-INF/js/index.js
@@ -1,3 +1,13 @@
 $(function() {
-
+    $.ajax({
+        type:"post",
+        url:"",
+        dataType:"json",
+        data:{
+            
+        },
+        success:function (data) {
+            
+        }
+    });
 });
